b5d1309462e940b834a8bd1c450d20b59b1cde69
Hiroaki Tahara <taharahiroaki@machiro.local>
2025-06-05 23:09:54 +0900

    api-gateway to auth-service ok

--- api-gateway/cmd/main.go
+++ api-gateway/cmd/main.go
@@ -1,15 +1,90 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
 	"net/http"
 
 	"github.com/hiroaki-th/response"
 )
 
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/authenticate", Authenticate())
+	mux.HandleFunc("/sign-up", SignUp())
+	mux.HandleFunc("/ping", Ping())
+	// mux.HandleFunc("/handle", Handler())
 
+	err := http.ListenAndServe(":8081", mux)
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
-func Handle(w http.ResponseWriter, r *http.Request) {
-	response.Accepted(w).Return()
+func Authenticate() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		Post(w, r, "http://auth-service:8080/authenticate")
+	}
+}
+
+func SignUp() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		Post(w, r, "http://auth-service:8080/sign-up")
+	}
+}
+
+func Ping() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response.Accepted(w).
+			Json().
+			SetBody(
+				struct {
+					Message string `json:"message"`
+				}{
+					Message: "connection resolved",
+				}).
+			Return()
+	}
+}
+
+func Post(w http.ResponseWriter, r *http.Request, url string) {
+	client := &http.Client{}
+
+	rBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		response.BadRequest(w).SetError(err).Return()
+		return
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(rBody))
+	if err != nil {
+		response.BadRequest(w).SetError(err).Return()
+		return
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		response.BadRequest(w).SetError(err).Return()
+		return
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		response.BadRequest(w).SetError(err).Return()
+		return
+	}
+
+	var obj interface{}
+	err = json.Unmarshal(data, &obj)
+	if err != nil {
+		response.BadRequest(w).SetError(err).Return()
+		return
+	}
+
+	response.Success(w).
+		Json().
+		SetBody(obj).
+		Return()
 }
