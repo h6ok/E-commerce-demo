47b57cd107171c0d457035fcdbefa5b2ea35bec7
Hiroaki Tahara <taharahiroaki@machiro.local>
2025-08-17 23:56:22 +0900

    toast

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -8,23 +8,37 @@ import Women from "./feature/women/Women";
 import Kids from "./feature/kids/Kids";
 import Notifications from "./feature/notifications/Notifications";
 import User from "./feature/user/User";
+import Toast from "./components/Toast";
+import { ToastContext, ToastProvider } from "./context/ToastContext";
+import { useContext } from "react";
 
 function App() {
+  const toastContext = useContext(ToastContext);
+
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/notifications" element={<Notifications />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/cart" element={<div>cart</div>} />
-            <Route path="/men" element={<Men />} />
-            <Route path="/women" element={<Women />} />
-            <Route path="/kids" element={<Kids />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
+      <ToastProvider>
+        {toastContext?.isOpen && (
+          <Toast
+            title={toastContext.title}
+            message={toastContext.message}
+            variant={toastContext.variant}
+          />
+        )}
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/notifications" element={<Notifications />} />
+              <Route path="/user" element={<User />} />
+              <Route path="/cart" element={<div>cart</div>} />
+              <Route path="/men" element={<Men />} />
+              <Route path="/women" element={<Women />} />
+              <Route path="/kids" element={<Kids />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </ToastProvider>
     </BrowserRouter>
   );
 }
