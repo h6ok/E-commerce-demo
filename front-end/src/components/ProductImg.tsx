1b78d49890f4ee6fe4fd6dc17fadfb4db492ddb2
Hiroaki Tahara <taharahiroaki@machiro.local>
2025-09-04 23:18:32 +0900

    update

--- front-end/src/components/ProductImg.tsx
+++ front-end/src/components/ProductImg.tsx
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import ProductIcon from "../assets/product.png";
+import ProductDialog from "./ProductDialog";
 
 export type ProductImgProps = {
   name: string;
   price: number;
   category: string;
+  displayCategory?: boolean;
 };
 
-export default function ProductImg({ name, price, category }: ProductImgProps) {
+export default function ProductImg(props: ProductImgProps) {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="flex flex-col items-center justify-center shadow-xl rounded-lg bg-gray-200">
-      <div className="transition hover:-translate-y-1 cursor-pointer">
-        <img className="scale-75 z-0" src={ProductIcon} />
+    <>
+      <div className="flex flex-col items-center justify-center shadow-xl rounded-lg bg-gray-200 pb-5">
+        <div
+          className="transition hover:-translate-y-1 cursor-pointer"
+          onClick={() => setOpen(!open)}
+        >
+          <img className="scale-75 z-0" src={ProductIcon} />
+        </div>
+        <div className="text-center">{props.name}</div>
+        <div className="text-center">{`$ ${props.price}`}</div>
+        {props.displayCategory && <div>{props.category}</div>}
       </div>
-      <div className="text-center">{name}</div>
-      <div className="text-center">{`$ ${price}`}</div>
-      <div>{category}</div>
-    </div>
+      <ProductDialog product={props} open={open} />
+    </>
   );
 }
--- front-end/src/components/Slide.tsx
+++ front-end/src/components/Slide.tsx
@@ -1,24 +1,39 @@
+import { Link } from "react-router-dom";
 import type { ProductImgProps } from "./ProductImg";
 import ProductImg from "./ProductImg";
 
 export type SlideProps<T> = {
   content: T[];
-  title: string;
+  title?: string;
+  linkTo?: string;
+  linkLabel?: string;
+  displayCategory?: boolean;
 };
 
-export default function Slide({ content, title }: SlideProps<ProductImgProps>) {
+export default function Slide({
+  content,
+  title,
+  displayCategory,
+  linkTo,
+  linkLabel,
+}: SlideProps<ProductImgProps>) {
   return (
     <>
       <div className="text-4xl flex items-center justify-center pt-10">
         <div className="border-b-4 border-double"> {title} </div>
       </div>
       <div className="w-0.8 flex items-center justify-center pt-15">
-        <div className="grid grid-cols-3 gap-30 pb-20">
+        <div className="grid grid-cols-3 gap-30 pb-15">
           {content.map((c) => (
-            <ProductImg {...c} />
+            <ProductImg {...c} displayCategory={displayCategory} />
           ))}
         </div>
       </div>
+      {linkTo && (
+        <div className="text-xl flex justify-center items-center pb-10">
+          <Link to={`/${linkTo}`}>{linkLabel}</Link>
+        </div>
+      )}
     </>
   );
 }
